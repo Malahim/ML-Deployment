0c3359e349df2deb09ffe037e455c73e55f79351
Malahim <59058195+Malahim@users.noreply.github.com>
2023-01-28 21:16:23 +0500

    Update main.py

--- main.py
+++ main.py
@@ -41,6 +41,7 @@ try:
     with open('ModelPred.pkl', 'rb') as file:
         file_size = os.path.getsize('ModelPred.pkl')
         print(f'The file size of ModelPred.pkl is {file_size} bytessdsds')
+        data=file.read()
         model = pickle.Unpickler(file).load()
 except (pickle.UnpicklingError, EOFError, ImportError, IndexError) as e:
     print("An error occurred while unpickling the file:", e)
